cf082111584440057bc6d1fb71590bafde9bb082
Hoang Mirs <hoang.mirs@gmail.com>
2020-08-17 23:20:30 +0700

    Add loading spinner login screen

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Header />
+  <div class="w-screen h-screen">
+    <div class="spinner"></div>
   </div>
 </template>
 
@@ -11,3 +11,20 @@ export default {
   },
 }
 </script>
+
+<style lang="postcss" scoped>
+@keyframes spinner {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.spinner::before {
+  @apply box-border absolute top-1/2 left-1/2 w-6 h-6 rounded-full border-2;
+
+  content: '';
+  border: 2px solid #ccc;
+  border-top-color: #000;
+  animation: spinner 0.6s linear infinite;
+}
+</style>
